0c049c2417fb69dd1afe0ce9aace1335979eaf6c
Chan Yu Kiu Elkie <elkieykchan@gmail.com>
2023-05-28 14:01:33 +0800

    cleaned diagnosis table to remove those with prior autoimmune diseases

--- 01_wrangle.R
+++ 01_wrangle.R
@@ -36,36 +36,118 @@ bioo <- bioo_df$Agent
 cdmard <- c("Sulfasalazine", "Mycophenolate", "Methotrexate", "Leflunomide", "Hydroxychloroquine", "Cyclophosphamide", "Ciclosporin", "Azathioprine", "Apremilast")
 
 
-
 # clean diagnosis ------------------------------------------------------
 # firstly, only consider pt if diagnosed with RA at least at some point in life
-
 # the following line shows that other than rheumatoid arthritis, you also have rheumatoid nodule arthritis
 ra_vector <- unique(grep(
   pattern =
-    "rheumatoid|Rheumatoid|[Jj]uv.*rh",
+    "rheumatoid|Rheumatoid|[Jj]uv.*rh|714",
   x = diagnosis$All.Diagnosis.Description..HAMDCT..,
   value = TRUE
 ))
+
+# exclude this strange item called Screen-rheumatoid
+ra_vector <- grep(pattern = "^(?!.*Screen)", x = ra_vector, value = TRUE, perl = TRUE)
 
 # there are some strange diagnosis with rheum, and for the benefit of doubt (and discussion), they are as follows: now the decision is whether you would like to keep these? Could modify the expression above to do that
 # Find the strings that contain "rheum" but not in in ra_vector (which we will use for analysis)
 not_ra_vector <- unique(grep(
   pattern =
-    "rheum",
+    "rheum|RA",
   x = diagnosis$All.Diagnosis.Description..HAMDCT..,
   value = TRUE
 ))
 
 not_ra_vector[!not_ra_vector %in% ra_vector] # so these are the strings
 
+
+# for pt diagnosed with cancer or autoimmune disease before RA, remove them
+# for each pt diagnosed with RA, get the earliest reference date for the RA diagnosis
+# Group by patient and get earliest reference date
+earliest_ra <- diagnosis[diagnosis$All.Diagnosis.Description..HAMDCT.. %in% ra_vector, ] %>%
+  group_by(Reference.Key.) %>%
+  summarize(first_ra = min(Reference.Date.))
+
+# double check only
+# earliest_ra[earliest_ra$Reference.Key. == 5872, ]
+# diagnosis[diagnosis$Reference.Key. == 5872,]
+
+# update the table with a column on the earliest RA diagnosis if any
+# turns out all the pt given already diagnosed with RA duh!
+diagnosis_sub <- dplyr::left_join(x = diagnosis,
+                                  y = earliest_ra,
+                                  by = "Reference.Key.") %>%
+  filter(Reference.Date. < first_ra)
+
+
+# obtain a list of all biologics indications
+# later, when cleaning diagnosis df, we need to remove pt with diagnosis of ANY biologics indications prior to their first date of RA, in case they may have received biologics for a purpose other than RA. I need a string vector for those indications
+
+# taken from https://ard.bmj.com/content/annrheumdis/suppl/2021/10/22/annrheumdis-2021-221571.DC1/annrheumdis-2021-221571supp001_data_supplement.pdf  they are as follows:
+# reactive arthritis
+#   099\.3|
+#   711\.[13]|
+#   716\.[4569]|
+#   719\.4|
+#   # SLE
+#   710\.0|
+#   # SA
+#   720\.0|
+#   # psoriasis
+#   696|
+#   # multiple sclerosis
+#   340|
+#   55[56]|
+#   # neoplasm
+#   14[0-9]|15[0-9]|
+#   16[0-5]|
+#   17[0-2]|
+#   17[4-6]|
+#   179|
+#   18[0-9]|
+#   19[0-5]|
+#   20[0-9]
+
+other_biologic <- grepl(
+  x = diagnosis_sub$All.Diagnosis.Code..ICD9..,
+pattern = "099\\.3|
+711\\.[13]|
+716\\.[4569]|
+719\\.4|
+710\\.0|
+720\\.0|
+696|
+340|
+55[56]|
+14[0-9]|15[0-9]|
+16[0-5]|
+17[0-2]|
+17[4-6]|
+179|
+18[0-9]|
+19[0-5]|
+20[0-9]") # you need double backslashes in R, as \ is also escape character in R itself
+
+unwanted_ra <- diagnosis_sub[other_biologic, "Reference.Key."]
+
+diagnosis_sub <- diagnosis_sub %>% 
+  filter(!Reference.Key. %in% unwanted_ra)
+
+
+
+
+
+# clean inpatient ---------------------------------------------------------
+inpatient
+
+# clean presscription -----------------------------------------------------
+
+
+
+# rough -------------------------------------------------------------------
 # Filter the diagnosis table to only get pt who has, in their lifetime, been diagnosed with any in ra_vec
 ra_vector_refkey <- diagnosis[diagnosis$All.Diagnosis.Description..HAMDCT.. %in% ra_vector, "Reference.Key."]
 diagnosis_sub <- diagnosis[diagnosis$Reference.Key. %in% ra_vector_refkey, ]
-
-# for pt diagnosed with cancer or autoimmune disease before RA, remove them
-diagnosis
-
 
 colnames(prescription)
 colnames(inpatient)
